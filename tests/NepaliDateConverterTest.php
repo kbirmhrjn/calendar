a5ba8024b438d763a7c3cdfc306d3e0645c70bb1
unknown <kabeerdarocker@gmail.com>
2016-02-02 21:05:49 +0545

    first draft

---
+++ tests/NepaliDateConverterTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Kabir;
+
+use PHPUnit_Framework_TestCase;
+
+class NepaliDateConverterTest extends PHPUnit_Framework_TestCase
+{
+
+    /** @test */
+    public function should_be_able_to_convert_english_date_to_nepali()
+    {
+        $cal = new Calendar();
+        $nepaliDate = $cal->eng_to_nep(2008,11,23);
+        $this->assertEquals($nepaliDate['year'], 2065);
+        $this->assertEquals($nepaliDate['month'], 8);
+        $this->assertEquals($nepaliDate['date'], 8);
+        $this->assertEquals($nepaliDate['day'], "Sunday");
+        $this->assertEquals($nepaliDate['nmonth'], "Mangshir");
+    }
+
+    /** @test */
+    public function should_be_able_to_convert_nepali_date_to_english()
+    {
+        $cal = new Calendar();
+        $nepaliDate = $cal->nep_to_eng(2065,8,8);
+        $cal->eng_to_nep(2008,11,23);
+
+        $this->assertEquals($nepaliDate['year'], 2008);
+        $this->assertEquals($nepaliDate['month'], 11);
+        $this->assertEquals($nepaliDate['date'], 23);
+        $this->assertEquals($nepaliDate['day'], "Sunday");
+        $this->assertEquals($nepaliDate['emonth'], "November");
+    }
+}
